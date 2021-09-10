66e566124ad3577d05efd274b0cd9dd4aa2c8b95
Robbie Jakob-Whitworth <git@robbie.dev>
2021-09-10 10:06:14 +0100

    Add fonts and all styles

--- components/CVMarkup/index.js
+++ components/CVMarkup/index.js
@@ -5,18 +5,51 @@ import MarkdownText from "./MarkdownText";
 
 export default function CVMarkup({ data }) {
     return (
-        <div className="CV">
-            <main className="a4">
-                <Title>
-                    {data.title}
-                </Title>
-                <ProfileStatement>
-                    <MarkdownText>
-                        {data.profileStatement}
-                    </MarkdownText>
-                </ProfileStatement>
-                {data.sections && data.sections.map((section) => <Section data={section} key={section.sectionTitle} />)}
-            </main>
-        </div>
+        <>
+            <div className="CV">
+                <main className="a4">
+                    <Title>
+                        {data.title}
+                    </Title>
+                    <ProfileStatement>
+                        <MarkdownText>
+                            {data.profileStatement}
+                        </MarkdownText>
+                    </ProfileStatement>
+                    {data.sections && data.sections.map((section) => <Section data={section} key={section.sectionTitle} />)}
+                </main>
+            </div>
+            <style jsx>{`
+                .CV {
+                    padding: 50px;
+                }
+                
+                main {
+                    font-family: 'Open Sans', sans-serif;
+                    font-size: 10pt;
+                    padding: 20mm 25.4mm;
+                
+                    // Width and height in millimetres
+                    // (default)
+                    --page-width: 100;
+                    --page-height: 100;
+                
+                    // Ratio of millimetres to pixels, e.g. 0.5 means 210mm translates to 105px
+                    --base-scale: 1mm;
+                
+                    margin: auto;
+                    border: 1px solid #ccc;
+                    border-radius: 10px;
+                    width: calc(var(--page-width) * var(--base-scale));
+                    height: calc(var(--page-height) * var(--base-scale));
+                    box-sizing: border-box;
+                }
+                
+                main.a4 {
+                    --page-width: 210;
+                    --page-height: 297;
+                }
+            `}</style>
+        </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import CVSchemaBuilder from "../components/CVSchemaBuilder";
 import {useState} from "react";
 import Form from "@rjsf/material-ui";
 import CVMarkup from "../components/CVMarkup";
+import Head from "next/head";
 
 export default function Home() {
     let preloadedFormData;
@@ -27,6 +28,13 @@ export default function Home() {
 
     return (
         <>
+            <Head>
+                <title>CV Turbo</title>
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;0,800;1,400;1,700&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+            </Head>
             <div className="app">
                 {formData && <CVMarkup data={formData} />}
                 <Form
@@ -37,47 +45,6 @@ export default function Home() {
                     formData={formData}
                 />
             </div>
-            <style jsx>{`
-                .app {
-                    display: grid;
-                    grid-template-columns: auto auto 1fr 20px;
-                }
-                
-                .CV {
-                    padding: 50px;
-                }
-                
-                main {
-                    font-family: 'Open Sans', sans-serif;
-                    font-size: 10pt;
-                    padding: 20mm 25.4mm;
-                
-                    // Width and height in millimetres
-                    // (default)
-                    --page-width: 100;
-                    --page-height: 100;
-                
-                    // Ratio of millimetres to pixels, e.g. 0.5 means 210mm translates to 105px
-                    --base-scale: 1mm;
-                
-                    margin: auto;
-                    border: 1px solid #ccc;
-                    border-radius: 10px;
-                    width: calc(var(--page-width) * var(--base-scale));
-                    height: calc(var(--page-height) * var(--base-scale));
-                    box-sizing: border-box;
-                }
-                
-                main.a4 {
-                    --page-width: 210;
-                    --page-height: 297;
-                }
-                
-                .CVBuilderForm textarea {
-                        font-family: 'Roboto Mono', monospace;
-                        font-size: 80%;
-                }
-            `}</style>
             <style jsx global>{`
                 body {
                     margin: 0;
@@ -87,6 +54,16 @@ export default function Home() {
 
                 * {
                    box-sizing: border-box;
+                }
+                
+                .app {
+                    display: grid;
+                    grid-template-columns: auto auto 1fr 20px;
+                }
+                
+                .CVBuilderForm textarea {
+                        font-family: 'Roboto Mono', monospace;
+                        font-size: 80%;
                 }
             `}</style>
         </>
